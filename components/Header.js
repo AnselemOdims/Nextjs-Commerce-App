e31cd6e99e5c2afa84f10e3d773bc369a2e79f87
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2022-11-15 12:06:18 +0100

    Header initial styles

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="py-4 px-6 shadow-md">
+      <nav className="flex justify-between items-center">
+        <Link href="/" className="text-2xl font-bold">
+          Shopper
+        </Link>
+        <div className="flex gap-3">
+          <Link href="/cart">Cart</Link>
+          <Link href="/login">Login</Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Header from './Header';
 
 const Layout = ({ title, children }) => {
   return (
@@ -7,9 +8,11 @@ const Layout = ({ title, children }) => {
         <title>{title ? title + '- Shopper' : 'Shopper'}</title>
         <meta name="description" content="Shopper App" />
         <link rel="icon" href="/favicon.ico" />
-        <header>This is the header</header>
-        <main>{children}</main>
-        <footer>This is the footer</footer>
+        <div className="flex min-h-screen flex-col justify-between">
+          <Header />
+          <main className="container m-auto mt-4 px-4">{children}</main>
+          <footer className="flex justify-center items-center h-10 shadow-inner">This is the footer</footer>
+        </div>
       </Head>
     </>
   );
